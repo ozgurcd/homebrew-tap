b648098a5d7aa22b7e03238ad81c22ae0b33a77f
Ozgur Demir <ozgurcd@gmail.com>
2026-08-19 13:31:42 +0100

    rulefloor v0.2.0

--- Formula/rulefloor.rb
+++ Formula/rulefloor.rb
@@ -1,8 +1,8 @@
 class Rulefloor < Formula
   desc "Machine-checked rule ledger: pin invariants to the tests that prove them"
   homepage "https://github.com/ozgurcd/rulefloor"
-  url "https://github.com/ozgurcd/rulefloor/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "08c4446f27b9a6ec2f406de9afbe3ab9991169283eac056909dee53abb75f905"
+  url "https://github.com/ozgurcd/rulefloor/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "0b786b90caca8a57868ecee234f8a1c835809c39cd53ee28a330c085fbaae7f1"
   license "MIT"
 
   depends_on "go" => :build
