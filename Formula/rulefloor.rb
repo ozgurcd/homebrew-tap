f6e6f8516d1146d4ac626fa90f4ca7d932a36b62
Ozgur Demir <ozgurcd@gmail.com>
2026-08-27 17:47:42 +0100

    Update rulefloor to 0.7.0

--- Formula/rulefloor.rb
+++ Formula/rulefloor.rb
@@ -1,8 +1,8 @@
 class Rulefloor < Formula
   desc "Detect drift between declared invariants and bound checks"
   homepage "https://github.com/ozgurcd/rulefloor"
-  url "https://github.com/ozgurcd/rulefloor/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "59e90d0c58d4cbd197dec0d3dded21384a49f14edeaf048c5ec350b9a65293ab"
+  url "https://github.com/ozgurcd/rulefloor/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "a0b6ef8a212c64378a8aae9785bd8a589efa71daa42af3ce792827c70c4dc6c6"
   license "MIT"
 
   depends_on "go" => :build
@@ -13,6 +13,8 @@ class Rulefloor < Formula
 
   test do
     assert_match "rulefloor v#{version}", shell_output("#{bin}/rulefloor --version")
+    assert_match "rulefloor.version.v1", shell_output("#{bin}/rulefloor version --json")
+    assert_match "rulefloor.capabilities.v1", shell_output("#{bin}/rulefloor capabilities --json")
     system bin/"rulefloor", "init"
     assert_match "check OK", shell_output("#{bin}/rulefloor check")
   end
