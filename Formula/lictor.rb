20e7cf0ba944587bd3bfd80d21cea2ee0ec51104
Ozgur Demir <ozgurcd@gmail.com>
2026-09-21 12:40:55 +0100

    Use public Lictor release downloads
    Replace authenticated asset API URLs with public versioned archives. Preserve the four published checksums and the existing version test.

--- Formula/lictor.rb
+++ Formula/lictor.rb
@@ -6,40 +6,20 @@ class Lictor < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://api.github.com/repos/ozgurcd/lictor/releases/assets/574915641",
-          headers: [
-            "Accept: application/octet-stream",
-            "Authorization: Bearer #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN", "")}",
-            "X-GitHub-Api-Version: 2022-11-28",
-          ]
+      url "https://github.com/ozgurcd/lictor/releases/download/v0.1.0/lictor_0.1.0_darwin_arm64.tar.gz"
       sha256 "babb70fd8ff4acd0d72862a1f965ebea8461835194b299949e30a52430a5d5f4"
     else
-      url "https://api.github.com/repos/ozgurcd/lictor/releases/assets/574915642",
-          headers: [
-            "Accept: application/octet-stream",
-            "Authorization: Bearer #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN", "")}",
-            "X-GitHub-Api-Version: 2022-11-28",
-          ]
+      url "https://github.com/ozgurcd/lictor/releases/download/v0.1.0/lictor_0.1.0_darwin_amd64.tar.gz"
       sha256 "e21c93fa9f39c8707996c2fa8e0f1f89f41a763269abbe41c1fa18a73bab4af7"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://api.github.com/repos/ozgurcd/lictor/releases/assets/574915640",
-          headers: [
-            "Accept: application/octet-stream",
-            "Authorization: Bearer #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN", "")}",
-            "X-GitHub-Api-Version: 2022-11-28",
-          ]
+      url "https://github.com/ozgurcd/lictor/releases/download/v0.1.0/lictor_0.1.0_linux_arm64.tar.gz"
       sha256 "2d82acb9ed34d64f5d5feef03e9f1ecbefba307e4b671f6a1a5b7253864e36f0"
     else
-      url "https://api.github.com/repos/ozgurcd/lictor/releases/assets/574915643",
-          headers: [
-            "Accept: application/octet-stream",
-            "Authorization: Bearer #{ENV.fetch("HOMEBREW_GITHUB_API_TOKEN", "")}",
-            "X-GitHub-Api-Version: 2022-11-28",
-          ]
+      url "https://github.com/ozgurcd/lictor/releases/download/v0.1.0/lictor_0.1.0_linux_amd64.tar.gz"
       sha256 "c051b5fa995211de4b4910395dc10b46d7b7c05be32c6af87e65345bd4eca739"
     end
   end
