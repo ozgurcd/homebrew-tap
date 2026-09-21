2dd820c3707a99f15d5d10f3840616ed9bc9b6a2
Ozgur Demir <ozgurcd@gmail.com>
2026-09-21 13:56:59 +0100

    lictor v0.2.0

--- Formula/lictor.rb
+++ Formula/lictor.rb
@@ -1,26 +1,26 @@
 class Lictor < Formula
   desc "Identuum-specific gate execution"
   homepage "https://github.com/ozgurcd/lictor"
-  version "0.1.0"
+  version "0.2.0"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ozgurcd/lictor/releases/download/v0.1.0/lictor_0.1.0_darwin_arm64.tar.gz"
-      sha256 "babb70fd8ff4acd0d72862a1f965ebea8461835194b299949e30a52430a5d5f4"
+      url "https://github.com/ozgurcd/lictor/releases/download/v0.2.0/lictor_0.2.0_darwin_arm64.tar.gz"
+      sha256 "45044c79768fb6a7ba673e7a9cf00104a28b8970996861be432560317ca40813"
     else
-      url "https://github.com/ozgurcd/lictor/releases/download/v0.1.0/lictor_0.1.0_darwin_amd64.tar.gz"
-      sha256 "e21c93fa9f39c8707996c2fa8e0f1f89f41a763269abbe41c1fa18a73bab4af7"
+      url "https://github.com/ozgurcd/lictor/releases/download/v0.2.0/lictor_0.2.0_darwin_amd64.tar.gz"
+      sha256 "44d69c94d7d61a9d426cdf0e36af8b54490b09689e21d7b32be587c3f0711746"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ozgurcd/lictor/releases/download/v0.1.0/lictor_0.1.0_linux_arm64.tar.gz"
-      sha256 "2d82acb9ed34d64f5d5feef03e9f1ecbefba307e4b671f6a1a5b7253864e36f0"
+      url "https://github.com/ozgurcd/lictor/releases/download/v0.2.0/lictor_0.2.0_linux_arm64.tar.gz"
+      sha256 "bd7cd877e8ef34bd9ae991abffb5d905bd098ea78b261b4b88a72386f7132abf"
     else
-      url "https://github.com/ozgurcd/lictor/releases/download/v0.1.0/lictor_0.1.0_linux_amd64.tar.gz"
-      sha256 "c051b5fa995211de4b4910395dc10b46d7b7c05be32c6af87e65345bd4eca739"
+      url "https://github.com/ozgurcd/lictor/releases/download/v0.2.0/lictor_0.2.0_linux_amd64.tar.gz"
+      sha256 "d30e294ee962596d36eb8f9c86e1e84db46495059aa2d1c6ec19f529b8cdbf00"
     end
   end
 
@@ -31,6 +31,6 @@ class Lictor < Formula
   test do
     version_document = shell_output("#{bin}/lictor version --json")
     assert_match '"schema_version":"lictor.version.v1"', version_document
-    assert_match '"version":"v0.1.0"', version_document
+    assert_match '"version":"v0.2.0"', version_document
   end
 end
